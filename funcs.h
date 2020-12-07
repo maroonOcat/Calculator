ef41184c68843d8498e46c4ee5da25122caf946d
Yana Volkova <poisonoushumster@gmail.com>
2020-12-07 13:16:23 +0300

    features 1-4, funcs.h, main.cpp

--- funcs.h
+++ funcs.h
@@ -2,8 +2,8 @@
 #ifndef __FUNCS_H__ 
 #define __FUNCS_H__
 double Add(double, double);
-
+double Sub(double, double);
 double Mul(double, double);
-
+double Div(double, double);
 
 #endif // __FUNCS_H__
